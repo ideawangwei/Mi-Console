2a963bd7ec5ba1f8536902b8cd34406c45cf5d37
xiezg <1573623031@qq.com>
2017-08-29 13:49:46 +0800

    框架初始化

---
+++ src/main/java/org/ahstu/MiConsole/MiConsoleApplication.java
@@ -0,0 +1,12 @@
+package org.ahstu.MiConsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiConsoleApplication.class, args);
+	}
+}
